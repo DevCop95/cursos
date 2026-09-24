9ab5f42afc981904cc2dc67301eb139246babf7c
DevCop95 <yared.henriquezb@gmail.com>
2026-09-24 01:41:01 -0500

    v25: Footer simplificado y oculto en móvil (sin enlace a Perfil)

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *  - Fuentes de Google: caché primero (sus URLs son inmutables).
  *  - Google Identity, Supabase y demás orígenes: no se interceptan.
  */
-const VERSION = 'dev101x-v24';
+const VERSION = 'dev101x-v25';
 const STATIC_CACHE = `${VERSION}-static`;
 const FONT_CACHE = 'dev101x-fonts';
 
@@ -14,11 +14,11 @@ const PRECACHE = [
   './',
   './index.html',
   './manifest.json',
-  './css/tailwind.css?v=dev101x-v24',
-  './css/app.css?v=dev101x-v24',
-  './js/main.js?v=dev101x-v24',
-  './js/identicon.js?v=dev101x-v24',
-  './js/water-reveal.js?v=dev101x-v24',
+  './css/tailwind.css?v=dev101x-v25',
+  './css/app.css?v=dev101x-v25',
+  './js/main.js?v=dev101x-v25',
+  './js/identicon.js?v=dev101x-v25',
+  './js/water-reveal.js?v=dev101x-v25',
   './js/config.js',
   './js/state.js',
   './js/router.js',
